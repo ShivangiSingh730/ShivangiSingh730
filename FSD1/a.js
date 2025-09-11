cef9115b6134803f68145e7f4e50ec6ca3ec2261
Shivangi <pv8893148@gmail.com>
2025-09-11 13:04:14 +0530

    initial commit to flexwork

--- FSD1/a.js
+++
@@ -1,9 +0,0 @@
-// function abes(name)
-// {
-//     return "hello"+ name;
-// }
-// console.log(abes("welcome"));     //  ES5 function
-const abes = (name) =>{
-    return "hello"+name;
-}
-console.log(abes("Welcome"));    // ES6 function
